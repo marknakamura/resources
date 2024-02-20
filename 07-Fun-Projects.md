5c33d1129ce217bb6dcb6e7f4afda8cc1c02cbdb
laptop <laptop@lab.com>
2024-02-20 02:16:59 -0500

    Fixed missed FUn Projects

---
+++ 07-Fun-Projects.md
@@ -0,0 +1,7 @@
+07-Fun-Projects.md
+- XKCD Password Generator - Flexible and scriptable strong password generator
+  - https://github.com/redacted/XKCD-password-generator
+- TOFFEE Project - Collection of Linux-based open-source projects
+  - https://www.the-toffee-project.org/
+
+
--- breakout.py
+++ breakout.py
@@ -1,13 +1,21 @@
 def break_and_clean(text):
-            broken = text.split(" ")
-            cleaned = "-".join(broken).replace("*","").replace("\n","").replace(",","").replace("#","")
-            return cleaned
+    broken = text.split(" ")
+    cleaned = (
+        "-".join(broken)
+        .replace("*", "")
+        .replace("\n", "")
+        .replace(",", "")
+        .replace("#", "")
+    )
+    return cleaned
+
 
 def create_file(filename, text):
     with open(filename, "w") as file:
-         file.write(text)
-         return   
-    
+        file.write(text)
+        return
+
+
 def test_header(line):
     count_hashtags = 0
     range = 0
@@ -16,7 +24,7 @@ def test_header(line):
             count_hashtags = count_hashtags + 1
         range = range + 1
         if range > 4:
-             break
+            break
     if count_hashtags == 2:
         return "h2"
     if count_hashtags == 1:
@@ -24,7 +32,7 @@ def test_header(line):
     else:
         return False
 
-        
+
 with open("./Resources.md", "r") as file:
     keeping_count = 0
     text_to_add = ""
@@ -35,10 +43,10 @@ with open("./Resources.md", "r") as file:
             print(filename)
             print(text_to_add)
             if filename != "":
-                 create_file(filename, text_to_add)
-                # continue
+                create_file(filename, text_to_add)
+            # continue
             cleaned = break_and_clean(each)
-            keeping_count = keeping_count+1
+            keeping_count = keeping_count + 1
             if keeping_count < 10:
                 filename = "0" + str(keeping_count) + cleaned + ".md"
             else:
@@ -46,5 +54,4 @@ with open("./Resources.md", "r") as file:
             text_to_add = "" + filename + "\n"
         else:
             text_to_add = text_to_add + each
-
-
+    create_file(filename, text_to_add)
